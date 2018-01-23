b12cfabe7380a95f94246d61d0ede81febcd33d6
elight <kicknrudder@gmail.com>
2018-01-23 14:18:54 -0800

    Saving progress before dismantling completely

--- app.js
+++ app.js
@@ -3,9 +3,3 @@ const App = require('./lib/AppController');
 const app = new App;
 
 app.start();
-
-// process.on('SIGINT', function() {
-//     app.terminate();
-//     console.log('All GPIOs unexported');
-//     process.exit(0);
-// });
